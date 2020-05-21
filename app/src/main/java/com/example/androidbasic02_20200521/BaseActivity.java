7b5fc5ca9e17c4dc271b589b5b836efe7bdbe7c1
moonheuiwon <wonheuimoon@gmail.com>
2020-05-21 13:09:53 +0900

    사용자 정보화면 => BaseActivity로 상속

--- app/src/main/java/com/example/androidbasic02_20200521/BaseActivity.java
+++ app/src/main/java/com/example/androidbasic02_20200521/BaseActivity.java
@@ -1,6 +1,11 @@
 package com.example.androidbasic02_20200521;
+import android.content.Context;
+
 import androidx.appcompat.app.AppCompatActivity;
 public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
+
     public abstract void setupEvents();
     public abstract void setValues();
 }
--- app/src/main/java/com/example/androidbasic02_20200521/MainActivity.java
+++ app/src/main/java/com/example/androidbasic02_20200521/MainActivity.java
@@ -1,21 +1,38 @@
 package com.example.androidbasic02_20200521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.androidbasic02_20200521.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         setupEvents();
         setValues();
     }
 
     @Override
     public void setupEvents() {
+
+        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputName = binding.nameEdt.getText().toString();
+                Intent intent = new Intent(mContext, UserInfoActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
