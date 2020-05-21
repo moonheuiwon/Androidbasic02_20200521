3af7e60f51c518b454267fff6c420a79e34c472e
moonheuiwon <wonheuimoon@gmail.com>
2020-05-21 12:36:54 +0900

    프로젝트의 기본세팅 BaseActivity

---
+++ app/src/main/java/com/example/androidbasic02_20200521/BaseActivity.java
@@ -0,0 +1,6 @@
+package com.example.androidbasic02_20200521;
+import androidx.appcompat.app.AppCompatActivity;
+public abstract class BaseActivity extends AppCompatActivity {
+    public abstract void setupEvents();
+    public abstract void setValues();
+}
--- app/src/main/java/com/example/androidbasic02_20200521/MainActivity.java
+++ app/src/main/java/com/example/androidbasic02_20200521/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
     }
 }
